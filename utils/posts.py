ec34008136fde49c4d27034b5c16bffeff550a0c
Henry <hzhang5@stuy.edu>
2017-01-12 13:30:17 -0500

    added posts file in utils

---
+++ utils/posts.py
@@ -0,0 +1,6 @@
+def addPost(user, title, content):
+	db=sqlite3.connect('data/info.db')
+    c=db.cursor()
+    q="select "
+    postID=
+    q="INSERT INTO posts VALUES (\""+user+'\", \"'+myHashObj.hexdigest()+'\",' + "\"" + prefLang+"\",\""+ nativeLang + "\")"
