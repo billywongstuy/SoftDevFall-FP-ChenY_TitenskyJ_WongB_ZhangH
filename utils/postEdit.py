5cba6d98fc0a16350e4f4da7706248030d21ab36
yhzhan <hzhang5@stuy.edu>
2017-01-13 13:43:03 -0500

    Delete postEdit.py

--- utils/postEdit.py
+++
@@ -1,6 +0,0 @@
-def addPost(user, title, content):
-	db=sqlite3.connect('data/info.db')
-    c=db.cursor()
-    q="select "
-    postID=
-    q="INSERT INTO posts VALUES (\""+user+'\", \"'+myHashObj.hexdigest()+'\",' + "\"" + prefLang+"\",\""+ nativeLang + "\")"
