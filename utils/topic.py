073e6933037598c768ff1f9c8089b6da1b2b761f
Henry <hzhang5@stuy.edu>
2017-01-17 14:07:17 -0500

    added displayPost test

--- utils/topic.py
+++ utils/topic.py
@@ -1,7 +1,7 @@
 import csv
 import sqlite3
 
-def displayPost(language):
+def displayPosts():
 	db=sqlite3.connect('data/info.db')
 	c=db.cursor()
 	q="SELECT * FROM posts"
@@ -15,9 +15,27 @@ def displayPost(language):
 			temp.append(str(y))
 		posts.append(temp)
 
-	print posts
+	return posts
+	db.commit()
+	db.close()
+
+def selectPosts(language):
+	db=sqlite3.connect('data/info.db')
+	c=db.cursor()
+	q="SELECT * FROM posts"
+	c.execute(q)
+	var=c.fetchall()
+	print var
+	posts=[]
+	for x in var:
+		temp=[]
+		for y in x:
+			temp.append(str(y))
+		posts.append(temp)
+
+	return posts
 	db.commit()
 	db.close()
 	
-displayPost('eng')
+
 
