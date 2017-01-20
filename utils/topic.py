ba09fdad29e0a13722b7c9b1e5ae6a25b2285a66
Henry <hzhang5@stuy.edu>
2017-01-20 14:04:15 -0500

    fixed getLanguages

--- utils/topic.py
+++ utils/topic.py
@@ -20,11 +20,25 @@ def displayPosts():
 	db.commit()
 	db.close()
 
-def languages(username):
+#First item in returned list is the user's native language
+def getLanguages(username):
+	langs=[]
 	db=sqlite3.connect('data/info.db')
 	c=db.cursor()
-	q="SELECT prefLang FROM users WHERE username=\'"+username+"\'"
+	q="SELECT nativeLang FROM users WHERE username=\'"+username+"\'"
+	c.execute(q)
+	nativeLang=c.fetchall()[0][0]
+	langs.append(str(nativeLang))
 
+	q="SELECT prefLang FROM users WHERE username=\'"+username+"\'"
+	c.execute(q)
+	pref=c.fetchall()[0][0]
+	prefLangs=pref.split(',')
+	for x in prefLangs:
+		langs.append(str(x))
+	return langs
+
+#DOES NOT WORK YET ---- TAKES IN EITHER STRING OR LIST OF STRINGS (LANGUAGES)
 def selectPosts(language):
 	db=sqlite3.connect('data/info.db')
 	c=db.cursor()
@@ -41,6 +55,7 @@ def selectPosts(language):
 	db.commit()
 	db.close()
 	return posts
+
 	
 
 
