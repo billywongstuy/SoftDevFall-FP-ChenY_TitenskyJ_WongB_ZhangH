baba26745ffeb97e146afb0fa60cd9ccc170c8a3
Jessica <jtitensky@stuy.edu>
2017-01-25 06:43:37 +0000

    updated

--- app.py
+++ app.py
@@ -256,5 +256,5 @@ def getKeyboard(lang):
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-#    app.run()
+#    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run()
