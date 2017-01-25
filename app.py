4340ecc732c24654673c178634ead5b324661a1e
Jessica <jtitensky@stuy.edu>
2017-01-25 06:38:42 +0000

    change langs

--- app.py
+++ app.py
@@ -93,6 +93,20 @@ def makeEdit():
     print e
     return render_template("viewPost.html",post=p,comments=c,edits=e)    
 
+@app.route("/changeLang",methods=['POST'])
+def changeLang():
+    if not 'username' in session:
+        return redirect('/')
+    print "RRRRRRR"
+    print request.form
+    x=topic.getLanguages(session['username'])
+    y=[]
+    for k in x:
+        if k!=x[0]:
+            y.append(request.form[k])
+    auth.changeLanguages(y,session['username'])
+    return redirect('/account')
+
 
 @app.route("/account")
 def account():
@@ -242,5 +256,5 @@ def getKeyboard(lang):
 
 if __name__ == "__main__":
     app.debug = True
-#    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+#    app.run()
